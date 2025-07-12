8c7659a86473b9d5ceb344260f8487ed1bbe5bac
soluren <soluren@proton.me>
2025-07-12 16:39:14 +0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const tg = Telegram.WebApp;
-
-tg.ready()
-
-const userData = {
-  initData: Telegram.WebApp.initData,
-  initDataUnsafe: Telegram.WebApp.initDataUnsafe,
-  theme: Telegram.WebApp.themeParams,
-  platform: Telegram.WebApp.platform,
-};
-
-tg.sendData(JSON.stringify(userData));
+if (
+    typeof Telegram === 'undefined' ||
+    typeof Telegram.WebApp === 'undefined'
+  ) {
+    document.body.innerHTML = "<h2>Пожалуйста, откройте этот сайт через Telegram-бота.</h2>";
+  } else {
+    Telegram.WebApp.ready();
+    console.log("Добро пожаловать из Telegram!");
+  }
